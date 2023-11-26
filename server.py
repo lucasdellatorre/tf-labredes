8952a6c3ea11bf4c930af66ff9811a249369cd3b
Gabriel-Tabajara <gataba.santos@gmail.com>
2023-11-26 17:57:26 -0300

    comentando prints do conteudo dos pacotes

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ arp_count = 0
 
 def flooding_checker():
     global arp_request_count, arp_reply_count, icmp_count, ipv4_count, ipv6_count, icmpv6_count, udp_count, tcp_count, arp_count
-    print("OI")
+    # print("OI")
     while True:
         time.sleep(5)  # Interval time for checking flooding (adjust as needed)
         
@@ -56,7 +56,7 @@ threading.Thread(target=flooding_checker).start()
 server_socket = socket(AF_PACKET, SOCK_RAW, ntohs(0x0003))
 print("The server is ready to receive")
 while True:
-    print('-')
+    # print('-')
     frame = server_socket.recvfrom(2048)
     eHeader = frame[0][0:14]
     eth_hdr = struct.unpack("!6s6s2s", eHeader)  # 6 dest MAC, 6 host MAC, 2 ethType
@@ -75,22 +75,22 @@ while True:
         target_mac = binascii.hexlify(arp_hdr[7]).decode('utf-8')
         target_ip = inet_ntoa(arp_hdr[8])
 
-        print("ARP Packet:")
-        print(f"Source MAC: {source_mac}")
-        print(f"Source IP: {source_ip}")
-        print(f"Target MAC: {target_mac}")
-        print(f"Target IP: {target_ip}")
+        # print("ARP Packet:")
+        # print(f"Source MAC: {source_mac}")
+        # print(f"Source IP: {source_ip}")
+        # print(f"Target MAC: {target_mac}")
+        # print(f"Target IP: {target_ip}")
 
         arp_count += 1
 
         if arp_operation == 1:  # 1 is request, 2 is reply
-            print("ARP Request")
+            # print("ARP Request")
             arp_request_count += 1
         elif arp_operation == 2:
-            print("ARP Reply")
+            # print("ARP Reply")
             arp_reply_count += 1
-        else:
-            print("Unknown ARP Operation")       
+        # else:
+        #     print("Unknown ARP Operation")       
 
     elif eth_type == '86dd':  # Check if the Ethernet type is IPv6 (0x86DD)
         ipv6_count += 1
@@ -106,15 +106,15 @@ while True:
         source_address = inet_ntop(AF_INET6, ip_hdr[4])  # Source IPv6 Address
         destination_address = inet_ntop(AF_INET6, ip_hdr[5])  # Destination IPv6 Address
 
-        print("IPv6 Packet:")
-        print(f"Version: {version}")
-        print(f"Traffic Class: {traffic_class}")
-        print(f"Flow Label: {flow_label}")
-        print(f"Payload Length: {payload_length}")
-        print(f"Next Header: {next_header}")
-        print(f"Hop Limit: {hop_limit}")
-        print(f"Source IP address: {source_address}")
-        print(f"Destination IP address: {destination_address}")
+        # print("IPv6 Packet:")
+        # print(f"Version: {version}")
+        # print(f"Traffic Class: {traffic_class}")
+        # print(f"Flow Label: {flow_label}")
+        # print(f"Payload Length: {payload_length}")
+        # print(f"Next Header: {next_header}")
+        # print(f"Hop Limit: {hop_limit}")
+        # print(f"Source IP address: {source_address}")
+        # print(f"Destination IP address: {destination_address}")
 
         if next_header == 58:  # Check if the Next Header is ICMPv6 (58 for ICMPv6)
             icmpv6_data = frame[0][54:]  # Extract ICMPv6 data from the frame
@@ -124,10 +124,10 @@ while True:
             icmpv6_code = icmpv6_hdr[1]
             icmpv6_checksum = icmpv6_hdr[2]
 
-            print("ICMPv6 Packet:")
-            print(f"Type: {icmpv6_type}")
-            print(f"Code: {icmpv6_code}")
-            print(f"Checksum: {icmpv6_checksum}")
+            # print("ICMPv6 Packet:")
+            # print(f"Type: {icmpv6_type}")
+            # print(f"Code: {icmpv6_code}")
+            # print(f"Checksum: {icmpv6_checksum}")
             # Add handling for specific ICMPv6 types and codes as needed
 
     else:
@@ -140,26 +140,26 @@ while True:
         source_address = inet_ntoa(ip_hdr[8])
         destination_address = inet_ntoa(ip_hdr[9])
 
-        if version == 4:
-            print("IPv4")
-        elif version == 6:
-            print("IPv6")
-        else:
-            print("Other network protocol version (%d)" % version)
+        # if version == 4:
+        #     print("IPv4")
+        # elif version == 6:
+        #     print("IPv6")
+        # else:
+        #     print("Other network protocol version (%d)" % version)
 
-        print("Source IP address %s" % source_address)  # network to ascii conversion
-        print("Destination IP address %s" % destination_address)  # network to ascii conversion
+        # print("Source IP address %s" % source_address)  # network to ascii conversion
+        # print("Destination IP address %s" % destination_address)  # network to ascii conversion
 
         if protocol == 6:
-            print("TCP")
+            # print("TCP")
             tcp_count += 1
         elif protocol == 17:
-            print("UDP")
+            # print("UDP")
             udp_count += 1
         elif protocol == 1:
-            print("ICMP")
+            # print("ICMP")
             if version == 6:
                 icmpv6_count += 1
             icmp_count += 1
-        else:
-            print("Other transport protocol code (%d)" % protocol)
+        # else:
+        #     print("Other transport protocol code (%d)" % protocol)
